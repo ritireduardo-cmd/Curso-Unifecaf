e7aa92640e8b98b5b0076d7fa145b80e2700c8be
ritireduardo-cmd <ritireduardo@gmail.com>
2026-09-15 17:25:49 -0300

    docs: comentario na funcao calcular total

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ function calcularTotal (itens) {
     for (let i = 0; i < itens.length; i++) {
         total += itens[i].preco
     }
-    
+
+    // aplica desconto de fidelidade
+    // antes de retornar o valor final
+
     return total
 }
